31c6a39653f2b562f15a83fad91c3eaf2dbbd7f1
devguilhrm <guilimadev19@gmail.com>
2026-09-07 20:46:33 -0300

    feat(wallet): add wallet creation core use case with a handler implements

--- src/main/java/br/guirre/WalletAPI/core/useCase/CreateWalletHandler.java
+++ src/main/java/br/guirre/WalletAPI/core/useCase/CreateWalletHandler.java
@@ -1,4 +1,20 @@
 package br.guirre.WalletAPI.core.useCase;
 
-public class CreateWalletHandler {
+import br.guirre.WalletAPI.core.domain.Wallet;
+import br.guirre.WalletAPI.core.port.in.CreateWalletCase;
+import br.guirre.WalletAPI.core.port.in.CreateWalletCommand;
+import br.guirre.WalletAPI.core.port.out.SaveWalletPort;
+
+public class CreateWalletHandler implements CreateWalletCase {
+    private final SaveWalletPort saveWalletPort;
+
+    public CreateWalletHandler(SaveWalletPort saveWalletPort) {
+        this.saveWalletPort = saveWalletPort;
+    }
+
+    @Override
+    public Wallet execute(CreateWalletCommand command) {
+        Wallet wallet = Wallet.create(command.ownerId(), command.currency());
+        return saveWalletPort.save(wallet);
+    }
 }
