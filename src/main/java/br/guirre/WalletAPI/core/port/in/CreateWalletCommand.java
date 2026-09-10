2778aeb6a17a9f4eace3a07600af6bc7f0145e59
devguilhrm <guilimadev19@gmail.com>
2026-09-09 22:55:51 -0300

    feat(core): validate create wallet command

--- src/main/java/br/guirre/WalletAPI/core/port/in/CreateWalletCommand.java
+++ src/main/java/br/guirre/WalletAPI/core/port/in/CreateWalletCommand.java
@@ -3,5 +3,11 @@ package br.guirre.WalletAPI.core.port.in;
 import br.guirre.WalletAPI.core.domain.OwnerId;
 
 import java.util.Currency;
+import java.util.Objects;
 
-public record CreateWalletCommand(OwnerId ownerId, Currency currency) { }
+public record CreateWalletCommand(OwnerId ownerId, Currency currency) {
+    public CreateWalletCommand {
+        Objects.requireNonNull(ownerId);
+        Objects.requireNonNull(currency);
+    }
+}
